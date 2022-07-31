d75a05e8d8a48824d4ccfeefd519aa2defdd5067
Vova <vova@MacBook-Pro-Yana.local>
2022-07-31 21:27:58 +0300

    first page module added

---
+++ app/code/Krasil/FirstPage/Controller/Page/View.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Krasil\FirstPage\Controller\Page;
+
+use Magento\Framework\Controller\Result\Json;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
+
+class View extends Action
+{
+public function execute()
+{
+    /** @var Json $jsonResult */
+    $jsonResult = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+    $jsonResult->setData([
+        'message'=>'My First Page'
+        ]);
+    return $jsonResult;
+}
+}
