293c1ccc49b770868072abc8194772a18979af52
Vova <vova@MacBook-Pro-Yana.local>
2022-07-31 16:11:22 +0300

    Hello World module

---
+++ app/code/Krasil/Magento2Module/Controller/Index/index.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Krasil\Magento2Module\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends Action implements HttpGetActionInterface
+{
+    public function execute()
+    {
+        /** @var \Magento\Framework\View\Result\Page $page */
+        $page = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        return $page;
+    }
+}
